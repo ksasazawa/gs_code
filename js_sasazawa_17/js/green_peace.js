839867ebe459c05d3e2167c8666fe1a3098868d6
ksasazawa <kentasasazawa@gmail.com>
2022-10-22 21:03:46 +0900

    修正

--- js_sasazawa_17/js/green_peace.js
+++ js_sasazawa_17/js/green_peace.js
@@ -18,24 +18,25 @@ function init(){
     if(param=="?zu"){
         document.querySelector("#enemy_name").innerHTML = "ズ・ゴオマ・グ";
         document.querySelector("#enemy_photo").src = "./img/ズ・ゴオマ・グ_2.jpeg";
-        seconds = 3;
+        seconds = 2.5;
     }
     else if(param=="?me"){
         document.querySelector("#enemy_name").innerHTML = "メ・ガルメ・レ";
         document.querySelector("#enemy_photo").src = "./img/メ・ガルメ・レ_2.jpeg";
-        seconds = 2;
+        seconds = 1.5;
     }
     else if(param=="?go"){
         document.querySelector("#enemy_name").innerHTML = "ゴ・ガドル・バ";
         document.querySelector("#enemy_photo").src = "./img/ゴ・ガドル・バ_2.jpeg";
-        seconds = 1;
+        // document.querySelector("html").style.backgroundImage = `url("./img/forest.jpeg")`;
+        seconds = 1.5;
     }
     else if(param=="?n"){
         document.querySelector("#enemy_name").innerHTML = "ン・ダグバ・ゼバ";
         document.querySelector("#enemy_photo").src = "./img/ン・ダグバ・ゼバ_2.jpeg";
         document.querySelector("#your_photo").src = "./img/アルティメット.jpeg";
-        document.querySelector("html").style.backgroundColor = "white";
-        seconds = 0.5;
+        document.querySelector("html").style.backgroundImage = `url("./img/snow.jpeg")`;
+        seconds = 1;
         // document.querySelector("html").style.backgroundImage = "./img/snow.jpeg";
     }
 };
@@ -50,11 +51,12 @@ function janken(your_hand){
     let conclusion = "";
     document.querySelector("#enemy_rinrin").innerHTML = "";
     document.querySelector("#your_rinrin").innerHTML = "";
-    document.querySelector("#gu").style.opacity = 0.2;
-    document.querySelector("#cho").style.opacity = 0.2;
-    document.querySelector("#pa").style.opacity = 0.2;
+    document.querySelector("#gu").style.opacity = 0.3;
+    document.querySelector("#cho").style.opacity = 0.3;
+    document.querySelector("#pa").style.opacity = 0.3;
     // 自分の手を固定
-    document.querySelector("#"+your_hand).style.opacity = 0.9;
+    let your_hand_action = document.querySelector("#"+your_hand);
+    your_hand_action.style.opacity = 0.9;
     // 敵の手を設定
     const e = Math.ceil(Math.random()*3);
     switch(e){
@@ -76,23 +78,20 @@ function janken(your_hand){
     // あいこの時
     if(enemy_hand==your_hand){
         conclusion = "draw";
-        document.querySelector("#echo").innerHTML = conclusion;
+        // document.querySelector("#echo").innerHTML = conclusion;
         // １回以上勝敗がついている場合
         if(cnt>0){
-            // Donnが押されたらフラグ値をTrueにする。
+            // 【関数の定義】Donnが押されたらフラグ値をTrueにする。
             document.querySelector("#donn").addEventListener('click', function() {
                 flag = true;
                 console.log(flag);
             });
-            // 一定期間でボタンを消す。
+            // 【関数の定義】一定期間でボタンを消す。
             function closeButton() {
                 document.querySelector("#donn").style.display = "none"; // ボタンを消す
                 clearTimeout( timerId ); // タイマーを終了
              };
-            document.querySelector("#donn").style.display = "block";
-            timerId = setTimeout(closeButton , seconds*1000);
-            // ５秒プログラム停止
-            // sleep関数の定義
+            // 【関数の定義】sleep
             function sleep(waitSec, callbackFunc) {
                 // 経過時間（秒）
                 var spanedSec = 0;
@@ -108,17 +107,30 @@ function janken(your_hand){
                     }
                 }, 1000);           
             }
-            // sleep関数の実行          
+            // Donnボタンを表示し、タイマースタート。
+            const param = location.search;
+            if(param=="?go" || param=="?n"){
+                const x = Math.ceil(Math.random()*( 90 - 10 )) + 10;
+                const y = Math.ceil(Math.random()*( 90 - 10 )) + 10;
+                console.log(x+y);
+                document.querySelector("#donn").style.left = `${x}%`;
+                document.querySelector("#donn").style.top = `${y}%`;
+            };
+            document.querySelector("#donn").style.display = "block";
+            timerId = setTimeout(closeButton , seconds*1000);
+            // sleep関数の実行。５秒プログラム停止。
             sleep(seconds, function () {           
                 console.log(`${seconds}秒経過しました！`);
-                // 勝ちパターン
+                // 勝利パターン
                 if(flag==true){
                     console.log("勝利");
-                    document.querySelector("#echo").innerHTML = "大勝利！！";
+                    document.querySelector("html").style.backgroundColor = "pink";
+                    document.querySelector("#echo").innerHTML = "勝利！！";
                 }
-                // 負けパターン
+                // 敗北パターン
                 else{
                     console.log("敗北");
+                    document.querySelector("html").style.backgroundColor = "aqua";
                     document.querySelector("#echo").innerHTML = "敗北、、、";
                 };         
             });
@@ -127,17 +139,31 @@ function janken(your_hand){
     // 勝敗がついた時
     else{
         cnt += 1;
-        // 自分が負けの時
+        // 自分がじゃん負けの時
         if((enemy_hand=="gu" && your_hand=="cho") || (enemy_hand=="cho" && your_hand=="pa") || (enemy_hand=="pa" && your_hand=="gu")){
             conclusion = "lose";
-            document.querySelector("#echo").innerHTML = conclusion;
+            // document.querySelector("#echo").innerHTML = conclusion;
             document.querySelector("#enemy_rinrin").innerHTML = `${enemy_hand}rin,${enemy_hand}rin!`;
+            document.querySelector(".enemy_hand").animate( {
+                // marginTop : [ "0" , "10px", "0" ],
+                marginLeft : [ "0" , "20px", "0" ],
+            } , {
+                duration: 500,
+                iterations: 2
+            });
         }
-        // 自分が勝ちの時
+        // 自分がじゃん勝ちの時
         else if((enemy_hand=="gu" && your_hand=="pa") || (enemy_hand=="cho" && your_hand=="gu") || (enemy_hand=="pa" && your_hand=="cho")){
             conclusion = "win";
-            document.querySelector("#echo").innerHTML = conclusion;
+            // document.querySelector("#echo").innerHTML = conclusion;
             document.querySelector("#your_rinrin").innerHTML = `${your_hand}rin,${your_hand}rin!`;
+            your_hand_action.animate( {
+                // marginTop : [ "0" , "10px", "0" ],
+                marginRight : [ "0" , "20px", "0" ],
+            } , {
+                duration: 500,
+                iterations: 2
+            });
         }
     };
 };
